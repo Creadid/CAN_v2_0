057f8babdace6e8134fb0730e4021d28770894f2
JP <joao.paulo.aero@gmail.com>
2017-03-26 11:40:55 +0200

    commiting the first files to the trunk

---
+++ garbage_jp/jp_example/main.cpp
@@ -0,0 +1,14 @@
+//#include <QGuiApplication>
+//#include <QQmlApplicationEngine>
+#include<iostream>
+int main( void ) //int argc, char *argv[])
+{
+
+    std::cout << "ḧello world" << std::endl ;
+    //QGuiApplication app(argc, argv);
+
+    //QQmlApplicationEngine engine;
+    //engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    //return app.exec();
+}
