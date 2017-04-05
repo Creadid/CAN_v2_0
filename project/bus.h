8bc79971c0056c3078a75110a9a2ccffa7605907
João Paulo Silva <joao.paulo.aero@gmail.com>
2017-04-05 13:01:50 +0200

    BUS
    Updated the BUS functionality

--- project/bus.h
+++ project/bus.h
@@ -4,8 +4,27 @@
 
 class BUS
 {
+
+private:
+	
+	void BUS::f_Check_Nodes_Heatbeats( void );
+	void BUS::f_Update_Nodes( void );
+
+	// Setters and Getters
+	int  BUS::f_Get_Number_Of_Nodes(       ){ return m_Number_Of_Nodes ;}
+	void BUS::f_Set_Number_Of_Nodes( int x ){ m_Number_Of_Nodes = x    ;}
+
 public:
+
+	// Constructors and Destructors
     BUS();
+
+	// 5 Main Functions
+	//Init   ();
+	//Input  ();
+	Tick   ();
+	//Output ();
+	//Exit   ();
 };
 
 #endif // BUS_H
